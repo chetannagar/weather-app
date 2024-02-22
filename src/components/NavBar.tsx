8a0d739944f52cf89b3840d541cb2c147a008771
Chetan Nagar <51130466+chetannagar@users.noreply.github.com>
2024-02-22 12:40:04 +1100

    added .env variables file

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -27,7 +27,6 @@ export default function NavBar({ location }: Props) {
             try {
                 const response = await axios.get(
                     `https://api.openweathermap.org/data/2.5/find?q=${value}&appid=${API_KEY}`
-                    // `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
                 );
                 const suggestions = response.data.list.map((item: any) => item.name);
                 setSuggestions(suggestions);
